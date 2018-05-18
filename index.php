e86cae91acc64ac0c87811748a2afc9eabef49d1
Ousmane MAMA <mousmane@hotmail.com>
2018-05-18 09:39:45 +0200

    Première version du site

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Mon Site</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>Hello !!!</p>
+    </body>
+</html>
